94a926d8bb5777d02cb2823d842c521051fa70ed
Fernando Quisaguano <81866283+FerJeffQ@users.noreply.github.com>
2023-01-16 17:45:12 -0500

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ferjeff/Entorno_robot/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ferjeff/driver_diferencial/devel;/home/ferjeff/entorno_ws/devel;/opt/ros/noetic'.split(';') if '/home/ferjeff/driver_diferencial/devel;/home/ferjeff/entorno_ws/devel;/opt/ros/noetic' != '' else []
